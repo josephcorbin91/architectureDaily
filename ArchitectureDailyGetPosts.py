dd60d39a68b7b294a5994c21545896e1587930cf
josephcorbin91 <joseph.corbin91@yahoo.com>
2016-11-01 13:21:13 -0400

    Completed get_posts and get_threads

--- ArchitectureDailyGetPosts.py
+++ ArchitectureDailyGetPosts.py
@@ -1,53 +1,41 @@
 import scrapy, json
 
 from architectureDaily.items import ArchitecturedailyItem
+from pprint import pprint
 
 
 
 class ArchitectureSpider(scrapy.Spider):
 
 
-    json_data=open(file_directory).read()
-    data = json.loads(json_data)
-    pprint(data)
-    
-    
-    
-    city="London"
+    json_data=open('C:\\Python27\\Scripts\\architectureDaily\\architectureDaily\\threads.json').read()
+    data = json.loads(json_data) 
+   
     name = "get_posts"
     allowed_domains = ["skyscrapercity.com"]
-    start_urls = ["http://www.skyscrapercity.com/showthread.php?t=1697283&page=7"] 
-    nPage = 1
     
-
-    def parse(self, response):
-        global nPage
-        for sel in response.xpath('//td[contains(@id, "td_post")]'): 
-            item = ArchitecturedailyItem()        
-            item['id'] = ''.join(sel.xpath('normalize-space(@id)').extract())
-            #item['title'] = sel.xpath('normalize-space(div[contains(@id, "post_message")]/text() | div[contains(@id, "post_message")]//*/text())').extract()
-            item['title'] = ''.join(sel.xpath('normalize-space(div[contains(@id, "post_message")]/text())').extract())
-            item['image'] = sel.xpath('normalize-space(div[contains(@id, "post_message")]/img/@src)').extract_first(default='https://d13yacurqjgara.cloudfront.net/users/370014/screenshots/1738726/teste-4-drble_1x.jpg')
-            item['like'] = ''.join(sel.xpath('normalize-space(.//a[contains(@href, "thanks.php")]/text())').extract())
-            #if(len(item['like'])!=0):
-            #print item['like']
-            #if item.get('image','none') == 'none':
-			#    item['image'] = 'https://d13yacurqjgara.cloudfront.net/users/370014/screenshots/1738726/teste-4-drble_1x.jpg';
-            yield item
-            
-            """
-            To scrape mutliple pages 
-        next_page = response.xpath('//a[@rel="prev"]/@href').extract_first()
-
+ 
+  
         
-        print('nextpage')
-        print(next_page)
-        print('npage first')
-        print(nPage)
-        if next_page is not None and nPage != 5:
-            next_page = response.urljoin(next_page)
-            nPage+=1
-            print('npage second')
-            print(nPage)
-            yield scrapy.Request(next_page, callback=self.parse)
-           """
+    def start_requests(self):
+        for item in self.data: 
+            print('http://www.skyscrapercity.com/'+item['link'])
+            yield scrapy.Request('http://www.skyscrapercity.com/'+item['link'], self.parse)
+
+         
+    def parse(self, response):        
+        item = ArchitecturedailyItem()  
+        for post in response.xpath('//td[contains(@id, "td_post")]'):        
+            item['id'] = ''.join(post.xpath('normalize-space(@id)').extract())
+            #item['title'] = sel.xpath('normalize-space(div[contains(@id, "post_message")]/text() | div[contains(@id, "post_message")]//*/text())').extract()
+            item['title'] = ''.join(post.xpath('normalize-space(div[contains(@id, "post_message")]/text())').extract())
+            item['image'] = post.xpath('normalize-space(div[contains(@id, "post_message")]/img/@src)').extract_first()
+            item['like'] = ''.join(post.xpath('normalize-space(.//a[contains(@href, "thanks.php")]/text())').extract())
+        '''    
+        item['city'] = response.xpath('normalize-space(//meta[@name="keywords"]/@content)').extract_first().split(",")[0]
+        print("city")
+        print(item['city'])   
+      '''
+        yield item
+         
+         
